9414b61088d1793c84b8b547ee75651621ebf06e
Emmanuel Sanchez <fede@fede.website>
2022-08-11 12:33:36 -0500

    Lección 1 Kotlin: Variables y constantes

--- app/src/main/java/com/example/kotlinparaprincipiantes/MainActivity.kt
+++ app/src/main/java/com/example/kotlinparaprincipiantes/MainActivity.kt
@@ -9,7 +9,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         variablesYConstantes()
     }
+    /*
+    Aqui vamos ha hablar de constantes y variables
+    */
     private fun variablesYConstantes() {
+        // Variables
 
+        var myFirstVariable = "Hello Hackerman"
+        var myFirstNumber = 1
+
+        println(myFirstVariable)
+
+        myFirstVariable = "Bienvenidos"
+
+        println(myFirstVariable)
+
+        //myFirstVariable = 1
+
+        var mySecondVariable = "Suscribete"
+
+        println(mySecondVariable)
+
+        mySecondVariable = myFirstVariable
+
+        println(mySecondVariable)
+
+        myFirstVariable = "Ya te has suscrito"
+
+        println(myFirstVariable)
+
+        //Constante
+
+        val myFirstConstant = "Te ha gustado el tutorial?"
+        println(myFirstConstant)
+
+        //myFirstConstant = "Si te ha gustado dale like"
+
+        val mySecondConstant = myFirstVariable
+
+        println(mySecondConstant)
     }
 }
